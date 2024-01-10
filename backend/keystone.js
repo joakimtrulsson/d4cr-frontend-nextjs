e60f0c5466c8ca268b0d8a1cc81e31998b42e26e
joakimtrulsson <joakim.trulsson@icloud.com>
2024-01-10 12:41:39 +0100

    Cleaned up boilerplate

---
+++ backend/keystone.js
@@ -0,0 +1,16 @@
+import { config } from '@keystone-6/core';
+
+import { lists } from './schema';
+
+import { withAuth, session } from './auth';
+
+export default withAuth(
+  config({
+    db: {
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
+    },
+    lists,
+    session,
+  })
+);
