d6b53d33f61cbc5f144618d953699e075a882a2f
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-01-17 11:48:51 +0100

    chapter teaser completed

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,15 +2,27 @@ import '../styles/home.scss'
 import TextAndMedia from '../sections/text-media'
 import Item from '../database/sections-data/text-and-media-data'
 
+
+import ChapterTeaser from '../sections/chapter-teaser'
+
+
 export default function Home() {
   return (
     <main className='main'>
 
+      <ChapterTeaser />
+
+
+      { /* 
+
       {Item.map((item, key) => {
         return(
+          
           <TextAndMedia key={key} data={item} />
         )
       })}
+
+       */ }
 
     </main>
   )
