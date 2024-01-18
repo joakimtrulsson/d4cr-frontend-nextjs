9cefdc0a5146b295216972f88fb5ecdb45d4bb13
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-01-18 14:00:26 +0100

    waves completed

--- frontend/src/database/sections-data/text-and-media-data.js
+++ frontend/src/database/sections-data/text-and-media-data.js
@@ -1,10 +1,10 @@
 const Data = [
     {
-        'bgColor': 'yellow',
+        'bgColor': '#fff9eb',
         'isMediaLeft': true,
         'subHeading': 'WHAT WE DO',
-        'heading': 'Integrating UN Child Rights into Desig Worldwide',
-        'description': 'Lorem ipsum dolor sit amet consectetur. Fermentum sit hendrerit velit blandit lorem massa habitasse nis tortor.Purus eu malesuada curabitur eget donec pulvinar in sem.',
+        'heading': 'Integrating UN Child Rights into Desig Worldwide.',
+        'description': 'Lorem ipsum dolor sit amet consectetur. Fermentum sit hendrerit velit blandit lorem massa habitasse nis tortor. Purus eu malesuada curabitur eget donec pulvinar in sem.',
         'primaryBtnTitle': 'READ OUR DESIGN GUIDE',
         'primaryBtnUrl': '/primary',
         'secondaryBtnTitle': 'GET INVOLVED',
--- frontend/src/sections/text-media.js
+++ frontend/src/sections/text-media.js
@@ -1,5 +1,5 @@
-import TopWave from '../components/wave/top-wave'
-import BottomWave from '../components/wave/bottom-wave'
+import TopWave from '../components/waves/top-wave'
+import BottomWave from '../components/waves/bottom-wave'
 import Image from 'next/image'
 import DefaultImage from '../features/img-default.svg'
 import PrimaryButton from '../components/buttons/primary-button'
@@ -9,32 +9,33 @@ import '../styles/sections/text-and-media.scss'
 
 const TextAndMedia = ({ data }) => {
 
-    return(
+    return (
         <div className="text-and-media-section">
-            { /* <TopWave bgColor={data.bgColor} /> */ }
 
-                <div className="text-and-media-container">
+            <TopWave bgColor={data.bgColor} />
 
-                    <div className='text-content'>
-                        <p className='sub-heading'>{data.subHeading}</p>
-                        <h2>{data.heading}</h2>
-                        <p>{data.description}</p>
-                        <div className='button-container' >
-                            <PrimaryButton title={data.primaryBtnTitle} />
-                            <SecondaryButton title={data.secondaryBtnTitle} />
-                        </div>
+            <div className="text-and-media-container">
+
+                <div className='text-content'>
+                    <p className='sub-heading'>{data.subHeading}</p>
+                    <h2>{data.heading}</h2>
+                    <p>{data.description}</p>
+                    <div className='button-container' >
+                        <PrimaryButton title={data.primaryBtnTitle} />
+                        <SecondaryButton title={data.secondaryBtnTitle} />
                     </div>
-
-                    <div className='media-content'>
-                        {(data.imgUrl !== null && data.imgUrl !== undefined) ? 
-                        <Image className='media-image' src={data.imgUrl} /> 
-                        : 
-                        <Image className='media-image' src={DefaultImage} />}
-                    </div>
-
                 </div>
 
-                { /* <BottomWave bgColor={data.bgColor} /> */ }
+                <div className='media-content'>
+                    {(data.imgUrl !== null && data.imgUrl !== undefined) ?
+                        <Image className='media-image' src={data.imgUrl} />
+                        :
+                        <Image className='media-image' src={DefaultImage} />}
+                </div>
+
+            </div>
+
+            <BottomWave bgColor={data.bgColor} />
 
         </div>
     )
