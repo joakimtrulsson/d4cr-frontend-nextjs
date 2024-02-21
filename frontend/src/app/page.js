51abf1e1c8d1002692665f40b66154e189ac4b1f
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-02-21 10:50:53 +0100

    Buttons issue in Chapters resolved

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,17 +5,7 @@ import '../themes/sources/scss/base/utils.scss'
 export default function Home() {
   return (
     <main className='main'>
-
-      <p className='large-breakpoint'>LARGE BREAKPOINTS</p>
-      <h1>Heading 1</h1>
-      <h2>Heading 2</h2>
-      <h3>Heading 3</h3>
-      <h4>Heading 4</h4>
-      <p className='sub-heading-l'>SUB-HEADING L</p>
-      <p className='sub-heading-m'>SUB-HEADING M</p>
-      <p className='large-text'>Large text</p>
-      <p className='small-text'>Small text</p>
-      <p className='description'>Description</p>
+      <h1>hero</h1>
     </main>
   )
 }
