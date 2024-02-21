71ea0c8edf473853ab8c3c09da831aeb7e058e8f
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-02-21 10:57:27 +0100

    Anmination leaking in Accordion resolved

--- frontend/src/themes/components/accordion.js
+++ frontend/src/themes/components/accordion.js
@@ -10,14 +10,14 @@ export default function Accordion({ content }) {
     const [clickedValue, setClickedValue] = useState(0);
     const [selectedField, setSelectedField] = useState(content.fields[0]);
     const [documentKey, setDocumentKey] = useState(0); // Key to force re-rendering of DocumentRenderer
-    
+
     useEffect(() => {
         setSelectedField(content.fields[clickedValue]);
         setDocumentKey(clickedValue);
     }, [clickedValue, content.fields]);
 
     return (
-        <div className="accordion-container flex flex-column flex-justify-center flex-align-center 
+        <div className="accordion-container animation-background-container flex flex-column flex-justify-center flex-align-center 
         padding-tb--xxl">
 
             <div className='animation-background'>
@@ -44,14 +44,11 @@ export default function Accordion({ content }) {
                     </ul>
                 </div>
 
-                <div className='content-text bg-yellow-200 width--xxl padding--xl'>
+                <div className='content-text bg-yellow-200 width--xxl padding--xl '>
 
-                {selectedField.bodyText ? (
+                    {selectedField.bodyText && (
                         <DocumentRenderer key={documentKey} content={selectedField.bodyText} />
-                    ) : (
-                        <p>No content available</p>
-                    )}
-
+                    ) }
                 </div>
             </div>
         </div>
