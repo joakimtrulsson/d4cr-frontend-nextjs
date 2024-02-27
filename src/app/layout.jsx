aea3e042f328a4a6e6c092339903b4334d2fde41
Björn Bergström <bjorn.wiklund.bergstrom@outlook.com>
2024-02-27 13:37:30 +0100

    Navbar completed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import Head from 'next/head';
-import NavBar from '../themes/components/navbar'
-import { metadata } from '../data/metadata'
+import NavBar from '../themes/components/navbar';
+import { metadata } from '../data/metadata';
 
 export default function RootLayout({ children }) {
   return (
-     <html lang="en">
-       <Head>
-         <title>{metadata.title}</title>
-         <meta name="description" content={metadata.description} />
-         <link rel="stylesheet" href="../themes/sources/scss/app.scss" />
-       </Head>
-       <body>
-         <NavBar />
-         {children}
-         {/* <Footer /> */}
-       </body>
-     </html>
-   );
- }
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="stylesheet" href="../themes/sources/scss/app.scss" />
+      </Head>
+      <body>
+        <NavBar />
+        {children}
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
