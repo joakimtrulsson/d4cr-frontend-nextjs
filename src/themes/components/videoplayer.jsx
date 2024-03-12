a285ea5bf2e77844f80960efec8ef971be3807b2
Oskar Ku <oskar.kubicka@gmail.com>
2024-03-12 10:46:49 +0100

    hovereffect done on videoplayer

--- src/themes/components/videoplayer.jsx
+++ src/themes/components/videoplayer.jsx
@@ -1,21 +1,30 @@
-const VideoPlayer = ({video}) => {
+import React from 'react';
+import Image from 'next/image';
+import PlayBtn from "../sources/assets/graphics/buttons/play.png"
+import PlayBg from "../sources/assets/graphics/buttons/play-bg.png"
+
+const VideoPlayer = ({ video }) => {
 
     const videoData = video
     console.log(videoData)
     return (
-    <main>
-        {/* {console.log(videoData)} */}
-        <video className="video-player" width="320" height="260" controls>
-            <source src={videoData.url} type="video/mp4" />
-            <track
-                src="/path/to/captions.vtt"
-                kind="subtitles"
-                srcLang="en"
-                label="English"
-            />
-            Your browser does not support the video tag.
-        </video>
-    </main>
+        <main className="video-container">
+            {/* {console.log(videoData)} */}
+            <video className="video-player" width="320" height="260" controls>
+                <source src={videoData.url} type="video/mp4" />
+                <track
+                    src="/path/to/captions.vtt"
+                    kind="subtitles"
+                    srcLang="en"
+                    label="English"
+                />
+                Your browser does not support the video tag.
+            </video>
+            <button className="custom-play-button">
+                <Image className="play-btn" src={PlayBtn} width={80} height={80}/>
+                <Image className="play-bg"  src={PlayBg} width={80} height={80} />
+            </button>
+        </main>
     )
 }
 
