e55944980ba7a91caf7ade7a6728adb370fc60c1
Oskar Ku <oskar.kubicka@gmail.com>
2024-03-14 14:13:54 +0100

    case item slug klar

--- src/pages/cases/[slug].jsx
+++ src/pages/cases/[slug].jsx
@@ -1,22 +1,31 @@
 
 import { fetchCaseItemBySlug } from "../../graphql";
 import SectionRenderer from '../../themes/sources/js/section-renderer.js';
+import '../../themes/sources/scss/app.scss'
+import { DocumentRenderer } from '@keystone-6/document-renderer'
+import ResourcesSection from '../../themes/components/resource-section.jsx'
+import PrinciplesSection from '../../themes/components/principles.jsx'
 
 export default function CaseItem({ cases }) {
     console.log('cases', cases)
     return (
-        <main>
-            <div>
-                <h3>CASES</h3>
-                <h1>{cases.title}</h1>
-                <h2>{cases.preamble.document[0].children[0].text}</h2>
+        <main className="flex flex-column container-cases">
+            <div className="title-container">
+                <h4 className="sub-heading-m case">CASE</h4>
+                <h1 className="heading-1">{cases.title}</h1>
+                < DocumentRenderer document={cases.preamble.document} />
             </div>
-            <div>
-                {cases.sections.map((section, index) => (
-                    <SectionRenderer key={index} section={section} />
-
+            <div className="flex flex-column flex-align-center">
+                {cases.sections && cases.sections.map((section, index) => (
+                    <div className="renderer" >
+                        <SectionRenderer key={index} section={section} />
+                    </div>
                 ))
                 }
+                <div className="renderer">
+                    {cases.resources.resources.length != 0 ? (<ResourcesSection content={cases.resources} />) : null}
+                    {cases.principles.groups.length != 0 ? (<PrinciplesSection content={cases.principles} />) : null}
+                </div>
             </div>
 
         </main >
