77d77569999b768cdab5ab5170f1cfebf56b9d81
Oskar Ku <oskar.kubicka@gmail.com>
2024-04-08 17:12:06 +0200

    modal stuff, almost finished with functionality

--- src/themes/components/text-media-component.jsx
+++ src/themes/components/text-media-component.jsx
@@ -7,19 +7,25 @@ import Image from 'next/image'
 import { DocumentRenderer } from '@keystone-6/document-renderer'
 import getColorCode from '../sources/js/color-code.js'
 import { useState } from 'react'
-import ContactUsForm from './contact-us-form.jsx'
-//använda contactus form muterad eller göra ny modal
+import PopupForm from './popup-forms.jsx'
+
 export default function TextMediaComponent({ content }) {
-  
+
     const [isClicked, setIsClicked] = useState(false)
     const [slideOut, setSlideOut] = useState(false)
-    let shareIsTrue = false
-    console.log('content-url', content.cta2)
-    //se till så att även cta1 funkar att göra till popupen
+    let shareIsTrueCTA2 = false
+    let shareIsTrueCTA1 = false
+    //istället för www.popup.com kommer det troligen bli en fast fras- share eller slack
+    //se till så att man kan använda share eller slack direkt i komponenten type
     if (content.cta2 && content.cta2.url) {
-        console.log(content.cta2.url)
- 
-        shareIsTrue = true
+        console.log(content.cta2.url, 'share funkae')
+        
+        shareIsTrueCTA2 = content.cta2.url
+    }
+    if (content.cta1 && content.cta1.url ) {
+        console.log(content.cta1.url)
+
+        shareIsTrueCTA1 = content.cta1.url
     }
     function clickedVideo() {
 
@@ -67,27 +73,36 @@ export default function TextMediaComponent({ content }) {
                     <DocumentRenderer document={content.preamble} />
 
                     {(content.cta1 || content.cta2) && (
-
-                        <nav className='button-container flex flex-row flex-nowrap flex-justify-start flex-align-center 
+                        <>
+                            <nav className='button-container flex flex-row flex-nowrap flex-justify-start flex-align-center 
                             margin-tb--xxxs' >
 
-                            {content.cta1 && content.cta1.url && content.cta1.anchorText && ( /* primary button */
-                                <Link href={content.cta1.url} className='margin-r--xxxs'>
-                                    <PrimaryButton title={content.cta1.anchorText} />
-                                </Link>
-                            )}
+                                {content.cta1 && content.cta1.url && content.cta1.anchorText && ( /* primary button */
+                                    !shareIsTrueCTA1 ? (
+                                        <Link href={content.cta1.url} className='margin-r--xxxs'>
+                                            <PrimaryButton title={content.cta1.anchorText} />
+                                        </Link>) : (<SecondaryButton title={content.cta1.anchorText} onClick={clickedVideo} />)
 
-                            {content.cta2 && content.cta2.url && content.cta2.anchorText && ( /* secondary button */
-                                <>
-                                    {!shareIsTrue ?
-                                        <Link className='no-decoration' href={content.cta2.url}>
-                                            <SecondaryButton title={content.cta2.anchorText} />
-                                        </Link> : <SecondaryButton title={content.cta2.anchorText} onClick={clickedVideo} />
-                                    }
+                                )}
 
-                                </>
-                            )}
-                        </nav>
+                                {content.cta2 && content.cta2.url && content.cta2.anchorText && ( /* secondary button */
+                                    <>
+                                        {!shareIsTrueCTA2 ?
+                                            <Link className='no-decoration' href={content.cta2.url}>
+                                                <SecondaryButton title={content.cta2.anchorText} />
+                                            </Link> : <SecondaryButton title={content.cta2.anchorText} onClick={clickedVideo} />
+                                        }
+
+                                    </>
+                                )}
+                            </nav>
+                            <div className={` ${isClicked ? 'clicked' : 'not-clicked'} ${slideOut ? 'clicked-exit' : ''}`}>
+                                <div className={`modal flex flex-column flex-align-center ${slideOut ? 'slide-out' : ''}`}>
+                                    <button onClick={exitVideo} className="btn-exit-video">X</button>
+                                    <div className="box"><PopupForm type="share"/></div>
+                                </div>
+                            </div>
+                        </>
                     )}
 
                 </div>
@@ -96,12 +111,7 @@ export default function TextMediaComponent({ content }) {
                     {(content.image && content.image.url) && <Image className='center-image' src={content.image.url} alt={content.image.altText} fill={true} />}
                 </div>
 
-                <div className={` ${isClicked ? 'clicked' : 'not-clicked'} ${slideOut ? 'clicked-exit' : ''}`}>
-                        <div className={`modal flex flex-column flex-align-center ${slideOut ? 'slide-out' : ''}`}>
-                        <button onClick={exitVideo} className="btn-exit-video">X</button>
-                        <div className="box"><ContactUsForm/></div>
-                    </div>
-                </div>
+
             </div>
 
             {(content.border === 'BOTTOM' || content.border === 'TOPBOTTOM') && // bottom wave
