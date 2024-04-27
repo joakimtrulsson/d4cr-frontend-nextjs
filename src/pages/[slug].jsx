7b2da3b20fa3ba7b057ce5b601c46e73c9ffc70b
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-04-27 10:49:17 +0200

    minor change on slug page

--- src/pages/[slug].jsx
+++ src/pages/[slug].jsx
@@ -48,101 +48,96 @@ export default function SlugPage({ resolvedUrl }) {
     return notFound();
   }
   return (
-    <RootLayout tabTitle={title} resolvedUrl={resolvedUrl} language='en_GB'>
-      <main className='site-content flex flex-column flex-align-center flex-justify-start'>
-        {pageData?.title && <h1 className='heading-background'>{pageData.title}</h1>}
+    // <RootLayout tabTitle={title} resolvedUrl={resolvedUrl} language='en_GB'>
+    <main className='site-content flex flex-column flex-align-center flex-justify-start'>
+      {pageData?.title && <h1 className='heading-background'>{pageData.title}</h1>}
 
-        {(pageData?.heroPreamble ||
-          pageData?.ctaOneAnchorText ||
-          pageData?.ctaTwoUrlAnchorText) && (
-          <div className='flex flex-column flex-align-center flex-justify-center margin-b--xl width--m max-width-40 text-align-center'>
-            {pageData.heroPreamble && (
-              <DocumentRenderer document={pageData.heroPreamble.document} />
-            )}
+      {(pageData?.heroPreamble ||
+        pageData?.ctaOneAnchorText ||
+        pageData?.ctaTwoUrlAnchorText) && (
+        <div className='flex flex-column flex-align-center flex-justify-center margin-b--xl width--m max-width-40 text-align-center'>
+          {pageData.heroPreamble && (
+            <DocumentRenderer document={pageData.heroPreamble.document} />
+          )}
 
-            <nav className='flex flex-row'>
-              {pageData.ctaOneAnchorText &&
-                url1 &&
-                (url1 === 'share' || url1 === 'slack' ? (
-                  <>
-                    <PrimaryButton
-                      className='margin-r--xxs'
-                      title={pageData.ctaOneAnchorText}
-                      onClick={clickedBtnCTA1}
-                    />
-                  </>
-                ) : url1.startsWith('/') ? (
-                  <Link href={url1} passHref className='margin-lr--xxxs'>
-                    <PrimaryButton title={pageData.ctaOneAnchorText} />
-                  </Link>
-                ) : (
-                  <Link
-                    href={url1}
-                    className='margin-r--xxxs'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                  >
-                    <PrimaryButton title={pageData.ctaOneAnchorText} />
-                  </Link>
-                ))}
+          <nav className='flex flex-row'>
+            {pageData.ctaOneAnchorText &&
+              url1 &&
+              (url1 === 'share' || url1 === 'slack' ? (
+                <>
+                  <PrimaryButton
+                    className='margin-r--xxs'
+                    title={pageData.ctaOneAnchorText}
+                    onClick={clickedBtnCTA1}
+                  />
+                </>
+              ) : url1.startsWith('/') ? (
+                <Link href={url1} passHref className='margin-lr--xxxs'>
+                  <PrimaryButton title={pageData.ctaOneAnchorText} />
+                </Link>
+              ) : (
+                <Link
+                  href={url1}
+                  className='margin-r--xxxs'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <PrimaryButton title={pageData.ctaOneAnchorText} />
+                </Link>
+              ))}
 
-              {pageData.ctaTwoUrlAnchorText &&
-                url2 &&
-                (url2 === 'share' || url2 === 'slack' ? (
-                  <>
-                    <SecondaryButton
-                      className='margin-r--xxs'
-                      title={pageData.ctaTwoUrlAnchorText}
-                      onClick={clickedBtnCTA2}
-                    />
-                  </>
-                ) : url2.startsWith('/') ? (
-                  <Link href={url2} passHref className='no-decoration margin-lr--xxxs'>
-                    <SecondaryButton title={pageData.ctaTwoUrlAnchorText} />
-                  </Link>
-                ) : (
-                  <Link
-                    href={url2}
-                    className='margin-r--xxxs'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                  >
-                    <PrimaryButton title={pageData.ctaTwoUrlAnchorText} />
-                  </Link>
-                ))}
-            </nav>
-          </div>
-        )}
+            {pageData.ctaTwoUrlAnchorText &&
+              url2 &&
+              (url2 === 'share' || url2 === 'slack' ? (
+                <>
+                  <SecondaryButton
+                    className='margin-r--xxs'
+                    title={pageData.ctaTwoUrlAnchorText}
+                    onClick={clickedBtnCTA2}
+                  />
+                </>
+              ) : url2.startsWith('/') ? (
+                <Link href={url2} passHref className='no-decoration margin-lr--xxxs'>
+                  <SecondaryButton title={pageData.ctaTwoUrlAnchorText} />
+                </Link>
+              ) : (
+                <Link
+                  href={url2}
+                  className='margin-r--xxxs'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                >
+                  <PrimaryButton title={pageData.ctaTwoUrlAnchorText} />
+                </Link>
+              ))}
+          </nav>
+        </div>
+      )}
+      <div
+        className={` ${isClicked ? 'clicked' : 'not-clicked'} ${
+          slideOut ? 'clicked-exit' : ''
+        }`}
+      >
         <div
-          className={` ${isClicked ? 'clicked' : 'not-clicked'} ${
-            slideOut ? 'clicked-exit' : ''
+          className={`modal flex flex-column flex-align-center ${
+            slideOut ? 'slide-out' : ''
           }`}
         >
-          <div
-            className={`modal flex flex-column flex-align-center ${
-              slideOut ? 'slide-out' : ''
-            }`}
-          >
-            <button onClick={exitModal} className='btn-exit-video'>
-              X
-            </button>
-            <div className='box'>
-              {shareOrSlack === 'slack' && <SlackForm />}
-              {shareOrSlack === 'share' && <PopupForm />}
-            </div>
+          <button onClick={exitModal} className='btn-exit-video'>
+            X
+          </button>
+          <div className='box'>
+            {shareOrSlack === 'slack' && <SlackForm />}
+            {shareOrSlack === 'share' && <PopupForm />}
           </div>
         </div>
-        {pageData?.sections &&
-          pageData?.sections.map((section, index) => (
-            <section
-              key={index}
-              className='flex flex-column flex-align-center flex-justify-center'
-            >
-              <SectionRender key={index} section={section} />
-            </section>
-          ))}
-      </main>
-    </RootLayout>
+      </div>
+      {pageData?.sections &&
+        pageData?.sections.map((section, index) => (
+          <SectionRender key={index} section={section} />
+        ))}
+    </main>
+    // </RootLayout>
   );
 }
 
