0a6d7b45212028cb678d3eb373ba7063c98673ad
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-04-29 14:29:42 +0200

    Testing google analytics

--- src/components/MetaHeader/MetaHeader.jsx
+++ src/components/MetaHeader/MetaHeader.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import Icon from '../../styles/assets/graphics/d4cr-icon-OG.png';
 import { metadata } from '../../graphql/metaData';
 
@@ -16,44 +17,59 @@ export default function MetaHeader({ tabTitle, resolvedUrl, language }) {
       : `${tabTitle} | ${metadata.title}`;
 
   return (
-    <Head>
-      {tabTitle ? <title>{tabTitle}</title> : <title>{metadata.title}</title>}
+    <>
+      <Head>
+        {tabTitle ? <title>{tabTitle}</title> : <title>{metadata.title}</title>}
 
-      <link rel='canonical' href={fullUrl} />
+        <link rel='canonical' href={fullUrl} />
 
-      <link rel='icon' href='../icon.png' type='image/png' sizes='any' />
+        <link rel='icon' href='../icon.png' type='image/png' sizes='any' />
 
-      <link
-        rel='apple-touch-icon'
-        href='../apple-icon.png'
-        type='image/png'
-        sizes='any'
+        <link
+          rel='apple-touch-icon'
+          href='../apple-icon.png'
+          type='image/png'
+          sizes='any'
+        />
+
+        <meta name='description' content={metadata.description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta property='og:title' content={tabTitle} />
+        <meta property='og:site_name' content={metadata.title}></meta>
+        {fullUrl && <meta property='og:url' content={fullUrl} />}
+
+        {Icon && (
+          <>
+            <meta property='og:image' content={Icon}></meta>
+            <meta property='og:image:type' content='image/png' />
+            <meta property='og:image:alt' content="D4CR's icon"></meta>
+            <meta property='og:image:width' content='1200'></meta>
+            <meta property='og:image:height' content='630'></meta>
+          </>
+        )}
+
+        {language && <meta property='og:locale' content={language} />}
+
+        <meta name='twitter:card' content='summary_large_image'></meta>
+        <meta name='twitter:site' content='@D4C_Guide'></meta>
+        <meta
+          property='article:publisher'
+          content='https://www.facebook.com/designingforchildren/'
+        ></meta>
+      </Head>
+      <Script
+        strategy='afterInteractive'
+        src='https://www.googletagmanager.com/gtag/js?id=G-743F7N1YBF'
       />
 
-      <meta name='description' content={metadata.description} />
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <meta property='og:title' content={tabTitle} />
-      <meta property='og:site_name' content={metadata.title}></meta>
-      {fullUrl && <meta property='og:url' content={fullUrl} />}
-
-      {Icon && (
-        <>
-          <meta property='og:image' content={Icon}></meta>
-          <meta property='og:image:type' content='image/png' />
-          <meta property='og:image:alt' content="D4CR's icon"></meta>
-          <meta property='og:image:width' content='1200'></meta>
-          <meta property='og:image:height' content='630'></meta>
-        </>
-      )}
-
-      {language && <meta property='og:locale' content={language} />}
-
-      <meta name='twitter:card' content='summary_large_image'></meta>
-      <meta name='twitter:site' content='@D4C_Guide'></meta>
-      <meta
-        property='article:publisher'
-        content='https://www.facebook.com/designingforchildren/'
-      ></meta>
-    </Head>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-743F7N1YBF');
+        `}
+      </Script>
+    </>
   );
 }
