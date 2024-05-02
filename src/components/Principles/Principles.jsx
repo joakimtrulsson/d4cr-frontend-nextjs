369d44edd1372891d2ca83dc0cb0358e141951f0
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-05-02 10:58:25 +0200

    Fixed Principles section

--- src/components/Principles/Principles.jsx
+++ src/components/Principles/Principles.jsx
@@ -1,52 +1,75 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PrinciplesCard from '../PrinciplesCard/PrinciplesCard.jsx';
 
-export default function Principles({ content: chapterContent }) {
-  const chapterContentProp = chapterContent ? chapterContent : null;
+export default function Principles({ content }) {
+  const [sortedPrinciples, setSortedPrinciples] = useState([]);
+
+  useEffect(() => {
+    const sortedPrinciples = [...content.principles].sort((a, b) => {
+      if (a.principleCategory.title < b.principleCategory.title) {
+        return -1;
+      }
+      if (a.principleCategory.title > b.principleCategory.title) {
+        return 1;
+      }
+      return a.principleNumber.number - b.principleNumber.number;
+    });
+
+    const principlesByCategory = sortedPrinciples.reduce((acc, principle) => {
+      const categoryTitle = principle.principleCategory.title;
+      if (!acc[categoryTitle]) {
+        acc[categoryTitle] = [];
+      }
+      acc[categoryTitle].push(principle);
+      return acc;
+    }, {});
+
+    setSortedPrinciples(principlesByCategory);
+  }, [content]);
 
   return (
     <main className='flex flex-column flex-align-center principles margin-t--s'>
-      {chapterContentProp && chapterContentProp?.groups?.length > 0 ? (
+      {content && content?.principles?.length > 0 ? (
         <>
           <div className='text-align-center'>
-            <h2 className='margin-t--xxs margin--zero'>{chapterContentProp.title}</h2>
+            <h2 className='margin-t--xxs margin--zero'>{content.title}</h2>
             <p className='large-text margin-t--xxs'>
-              {chapterContentProp.preamble[0] && (
-                <>{chapterContentProp.preamble[0].children[0].text}</>
-              )}
+              {content.preamble[0] && <>{content.preamble[0].children[0].text}</>}
             </p>
           </div>
 
-          {chapterContentProp.groups.length > 0 ? (
+          {Object.entries(sortedPrinciples).length > 0 ? (
             <>
               <div
                 className={`full-width-height flex flex-column flex-wrap flex-justify-center flex-align-center
-                  }`}
+        }`}
               >
-                {chapterContentProp.groups.map((group, index) => (
-                  <div key={index}>
-                    <h4 className={'color-grey-400 text-align-center'}>
-                      {group.groupTitle}
-                    </h4>
-                    <div className='principles-container flex flex-row flex-wrap flex-justify-center flex-align-center'>
-                      {group.principles.map((principle) => (
-                        <PrinciplesCard
-                          key={principle.id}
-                          title={
-                            principle.principleNumber.number + '. ' + principle.title
-                          }
-                          subHeader={principle.subHeader}
-                          url={'/principles' + principle.slug}
-                          img={
-                            principle.image && principle.image.url
-                              ? principle.image.url
-                              : ''
-                          }
-                        />
-                      ))}
+                {Object.entries(sortedPrinciples).map(
+                  ([categoryTitle, principles], index) => (
+                    <div key={index}>
+                      <h4 className={'color-grey-400 text-align-center'}>
+                        {categoryTitle}
+                      </h4>
+                      <div className='principles-container flex flex-row flex-wrap flex-justify-center flex-align-center'>
+                        {principles.map((principle) => (
+                          <PrinciplesCard
+                            key={principle.id}
+                            title={
+                              principle.principleNumber.number + '. ' + principle.title
+                            }
+                            subHeader={principle.subHeader}
+                            url={'/principles' + principle.slug}
+                            img={
+                              principle.image && principle.image.url
+                                ? principle.image.url
+                                : ''
+                            }
+                          />
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  )
+                )}
               </div>
             </>
           ) : null}
