85c3d93f656317ec02628ac97ae08bfe8d3df62a
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-05-02 11:13:33 +0200

    Fixed sorting in Principles section

--- src/components/Principles/Principles.jsx
+++ src/components/Principles/Principles.jsx
@@ -4,18 +4,31 @@ import PrinciplesCard from '../PrinciplesCard/PrinciplesCard.jsx';
 export default function Principles({ content }) {
   const [sortedPrinciples, setSortedPrinciples] = useState([]);
 
-  useEffect(() => {
-    const sortedPrinciples = [...content.principles].sort((a, b) => {
-      if (a.principleCategory.title < b.principleCategory.title) {
-        return -1;
-      }
-      if (a.principleCategory.title > b.principleCategory.title) {
-        return 1;
-      }
-      return a.principleNumber.number - b.principleNumber.number;
-    });
+  // useEffect(() => {
+  //   const sortedPrinciples = [...content.principles].sort((a, b) => {
+  //     if (a.principleCategory.title < b.principleCategory.title) {
+  //       return -1;
+  //     }
+  //     if (a.principleCategory.title > b.principleCategory.title) {
+  //       return 1;
+  //     }
+  //     return a.principleNumber.number - b.principleNumber.number;
+  //   });
 
-    const principlesByCategory = sortedPrinciples.reduce((acc, principle) => {
+  //   const principlesByCategory = sortedPrinciples.reduce((acc, principle) => {
+  //     const categoryTitle = principle.principleCategory.title;
+  //     if (!acc[categoryTitle]) {
+  //       acc[categoryTitle] = [];
+  //     }
+  //     acc[categoryTitle].push(principle);
+  //     return acc;
+  //   }, {});
+
+  //   setSortedPrinciples(principlesByCategory);
+  // }, [content]);
+
+  useEffect(() => {
+    const principlesByCategory = content.principles.reduce((acc, principle) => {
       const categoryTitle = principle.principleCategory.title;
       if (!acc[categoryTitle]) {
         acc[categoryTitle] = [];
@@ -24,7 +37,15 @@ export default function Principles({ content }) {
       return acc;
     }, {});
 
-    setSortedPrinciples(principlesByCategory);
+    const categoriesSortedByMinNumber = Object.entries(principlesByCategory)
+      .map(([categoryTitle, principles]) => ({
+        categoryTitle,
+        minNumber: Math.min(...principles.map((p) => p.principleNumber.number)),
+      }))
+      .sort((a, b) => a.minNumber - b.minNumber)
+      .map(({ categoryTitle }) => categoryTitle);
+
+    setSortedPrinciples({ principlesByCategory, categoriesSortedByMinNumber });
   }, [content]);
 
   return (
@@ -44,31 +65,35 @@ export default function Principles({ content }) {
                 className={`full-width-height flex flex-column flex-wrap flex-justify-center flex-align-center
         }`}
               >
-                {Object.entries(sortedPrinciples).map(
-                  ([categoryTitle, principles], index) => (
-                    <div key={index}>
-                      <h4 className={'color-grey-400 text-align-center'}>
-                        {categoryTitle}
-                      </h4>
-                      <div className='principles-container flex flex-row flex-wrap flex-justify-center flex-align-center'>
-                        {principles.map((principle) => (
-                          <PrinciplesCard
-                            key={principle.id}
-                            title={
-                              principle.principleNumber.number + '. ' + principle.title
-                            }
-                            subHeader={principle.subHeader}
-                            url={'/principles' + principle.slug}
-                            img={
-                              principle.image && principle.image.url
-                                ? principle.image.url
-                                : ''
-                            }
-                          />
-                        ))}
+                {sortedPrinciples.categoriesSortedByMinNumber.map(
+                  (categoryTitle, index) => {
+                    const principles =
+                      sortedPrinciples.principlesByCategory[categoryTitle];
+                    return (
+                      <div key={index}>
+                        <h4 className={'color-grey-400 text-align-center'}>
+                          {categoryTitle}
+                        </h4>
+                        <div className='principles-container flex flex-row flex-wrap flex-justify-center flex-align-center'>
+                          {principles.map((principle) => (
+                            <PrinciplesCard
+                              key={principle.id}
+                              title={
+                                principle.principleNumber.number + '. ' + principle.title
+                              }
+                              subHeader={principle.subHeader}
+                              url={'/principles' + principle.slug}
+                              img={
+                                principle.image && principle.image.url
+                                  ? principle.image.url
+                                  : ''
+                              }
+                            />
+                          ))}
+                        </div>
                       </div>
-                    </div>
-                  )
+                    );
+                  }
                 )}
               </div>
             </>
