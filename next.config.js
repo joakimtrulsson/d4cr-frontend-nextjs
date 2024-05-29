c7e3a0e97e4efd5512fe7dfa4e60206ee6973662
Joakim Trulsson <joakim.trulsson@icloud.com>
2024-05-29 09:57:20 +0200

    Added preview image and hover animation to YoutubePlayer

--- next.config.js
+++ next.config.js
@@ -1,27 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-              protocol: "https",
-              hostname: "bucketeer-1f163294-7339-4eb0-af5c-400b0ce7209a.s3.eu-west-1.amazonaws.com",
-            },
-            {
-              protocol: "https",
-              hostname: "d4cr-keystone-19d55dc6f889.herokuapp.com",
-            },
-            {
-              protocol: "http",
-              hostname: "localhost",
-            },
-            {
-              protocol: "https",
-              hostname: "ryds.se", // delete this one!
-            },
-            // Add your image domains here
-        ],
-    },
-}
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:
+          'bucketeer-1f163294-7339-4eb0-af5c-400b0ce7209a.s3.eu-west-1.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd4cr-keystone-19d55dc6f889.herokuapp.com',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/YoutubeVideo/YoutubeVideo.jsx
+++ src/components/YoutubeVideo/YoutubeVideo.jsx
@@ -1,21 +1,82 @@
+import { useState } from 'react';
 import { getYouTubeEmbedId } from '../../utils';
+import Image from 'next/image';
+
+import PlayBtn from '../../styles/assets/graphics/buttons/play.png';
+import PlayBg from '../../styles/assets/graphics/buttons/play-bg.png';
 
 export default function YoutubeVideo({ url, altText = 'Embedded YouTube video' }) {
+  const [showVideo, setShowVideo] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [leaveTimeout, setLeaveTimeout] = useState(null);
   const embedId = getYouTubeEmbedId(url);
 
+  const handlePlay = () => {
+    setShowVideo(true);
+  };
+
+  const handleMouseEnter = () => {
+    if (leaveTimeout) {
+      clearTimeout(leaveTimeout);
+      setLeaveTimeout(null);
+    }
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    const timeoutId = setTimeout(() => {
+      setIsHovered(false);
+    }, 600);
+    setLeaveTimeout(timeoutId);
+  };
+
   return (
-    <iframe
-      className='youtube-video-iframe'
-      loading='lazy'
-      style={{
-        left: 0,
-        top: 0,
-      }}
-      src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder='0'
-      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-      allowFullScreen
-      title={altText}
-    />
+    <div
+      onClick={handlePlay}
+      className={`youtube-video-container ${isHovered ? 'hovered' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {showVideo ? (
+        <iframe
+          className='youtube-video-iframe'
+          style={{
+            left: 0,
+            top: 0,
+          }}
+          src={`https://www.youtube.com/embed/${embedId}?autoplay=1`}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+          title={altText}
+        />
+      ) : (
+        <div className='youtube-video-preview'>
+          <Image
+            layout='responsive'
+            height={315}
+            width={560}
+            src={`https://img.youtube.com/vi/${embedId}/maxresdefault.jpg`}
+            alt={altText}
+          />
+          <button className='custom-play-button'>
+            <Image
+              className='play-btn'
+              src={PlayBtn}
+              width={80}
+              height={80}
+              alt='the play button icon arrow'
+            />
+            <Image
+              className='play-bg'
+              src={PlayBg}
+              width={80}
+              height={80}
+              alt='the play button background'
+            />
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
